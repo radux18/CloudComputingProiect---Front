a2f51472a21432295e0b73a4a97e1f18a86538c2
Radu Stoica <radux188@gmail.com>
2022-05-13 18:08:05 +0300

    initial setup

--- src/javascript/components/MainPage.jsx
+++ src/javascript/components/MainPage.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function MainPage(props) {
     return (
         <div id="MainPage">
-
+            <h1 className="mt-10 text-red-400 text-4xl font-bold">Main Page</h1>
         </div>
     );
 }
